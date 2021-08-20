cd3480363d99d0a5f3b54ce7873fbb04f57df24a
Bryan Osorio <nacio2014@outlook.com>
2021-08-20 17:31:36 -0500

    Segundo formulario realizado

--- mockup1-848/Form2.Designer.cs
+++ mockup1-848/Form2.Designer.cs
@@ -29,134 +29,108 @@ namespace mockup1_848
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox8 = new System.Windows.Forms.GroupBox();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.groupBox8.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.pendiente = new System.Windows.Forms.Button();
+            this.finalizado = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.datos = new System.Windows.Forms.DataGridView();
+            this.groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datos)).BeginInit();
             this.SuspendLayout();
             // 
-            // groupBox8
+            // groupBox5
             // 
-            this.groupBox8.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.groupBox8.Controls.Add(this.button6);
-            this.groupBox8.Controls.Add(this.button5);
-            this.groupBox8.Controls.Add(this.button4);
-            this.groupBox8.Controls.Add(this.button3);
-            this.groupBox8.Location = new System.Drawing.Point(788, 36);
-            this.groupBox8.Name = "groupBox8";
-            this.groupBox8.Size = new System.Drawing.Size(217, 303);
-            this.groupBox8.TabIndex = 7;
-            this.groupBox8.TabStop = false;
+            this.groupBox5.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.groupBox5.Controls.Add(this.pendiente);
+            this.groupBox5.Controls.Add(this.finalizado);
+            this.groupBox5.Location = new System.Drawing.Point(1112, 49);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(156, 209);
+            this.groupBox5.TabIndex = 11;
+            this.groupBox5.TabStop = false;
             // 
-            // button6
+            // pendiente
             // 
-            this.button6.BackColor = System.Drawing.Color.Yellow;
-            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
-            this.button6.Location = new System.Drawing.Point(6, 172);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(205, 39);
-            this.button6.TabIndex = 88;
-            this.button6.Text = "Pendiente";
-            this.button6.UseVisualStyleBackColor = false;
+            this.pendiente.BackColor = System.Drawing.Color.Yellow;
+            this.pendiente.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.pendiente.Location = new System.Drawing.Point(6, 116);
+            this.pendiente.Name = "pendiente";
+            this.pendiente.Size = new System.Drawing.Size(144, 34);
+            this.pendiente.TabIndex = 2;
+            this.pendiente.Text = "MODIFICAR";
+            this.pendiente.UseVisualStyleBackColor = false;
+            this.pendiente.Click += new System.EventHandler(this.pendiente_Click);
             // 
-            // button5
+            // finalizado
             // 
-            this.button5.BackColor = System.Drawing.Color.Red;
-            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
-            this.button5.Location = new System.Drawing.Point(6, 98);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(205, 39);
-            this.button5.TabIndex = 87;
-            this.button5.Text = "Vencimiento";
-            this.button5.UseVisualStyleBackColor = false;
+            this.finalizado.BackColor = System.Drawing.Color.Lime;
+            this.finalizado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.finalizado.Location = new System.Drawing.Point(6, 40);
+            this.finalizado.Name = "finalizado";
+            this.finalizado.Size = new System.Drawing.Size(144, 34);
+            this.finalizado.TabIndex = 0;
+            this.finalizado.Text = "FINALIZADO ";
+            this.finalizado.UseVisualStyleBackColor = false;
+            this.finalizado.Click += new System.EventHandler(this.finalizado_Click);
             // 
-            // button4
+            // textBox1
             // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
-            this.button4.Location = new System.Drawing.Point(6, 248);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(205, 39);
-            this.button4.TabIndex = 86;
-            this.button4.Text = "Modificar";
-            this.button4.UseVisualStyleBackColor = true;
+            this.textBox1.Font = new System.Drawing.Font("Vladimir Script", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(1039, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(229, 31);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = "BUSCAR";
             // 
-            // button3
+            // button2
             // 
-            this.button3.BackColor = System.Drawing.Color.Lime;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(6, 19);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(205, 39);
-            this.button3.TabIndex = 85;
-            this.button3.Text = "Listo";
-            this.button3.UseVisualStyleBackColor = false;
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Font = new System.Drawing.Font("Vladimir Script", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(1039, 437);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(229, 34);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "REGISTRAR";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button1
+            // datos
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
-            this.button1.Location = new System.Drawing.Point(687, 602);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(205, 39);
-            this.button1.TabIndex = 89;
-            this.button1.Text = "Registrar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 35);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(770, 561);
-            this.dataGridView1.TabIndex = 8;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Listo",
-            "Vencimiento",
-            "Pendiente"});
-            this.comboBox1.Location = new System.Drawing.Point(788, 345);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(216, 33);
-            this.comboBox1.TabIndex = 90;
-            this.comboBox1.Text = "Filtrar por";
+            this.datos.BackgroundColor = System.Drawing.SystemColors.ScrollBar;
+            this.datos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datos.Location = new System.Drawing.Point(4, 49);
+            this.datos.Name = "datos";
+            this.datos.ReadOnly = true;
+            this.datos.Size = new System.Drawing.Size(1102, 382);
+            this.datos.TabIndex = 14;
+            this.datos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1112, 670);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.groupBox8);
+            this.ClientSize = new System.Drawing.Size(1305, 499);
+            this.Controls.Add(this.datos);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox5);
             this.Name = "Form2";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
-            this.groupBox8.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.datos)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.GroupBox groupBox8;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Button pendiente;
+        private System.Windows.Forms.Button finalizado;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView datos;
     }
 }
--- mockup1-848/Form2.cs
+++ mockup1-848/Form2.cs
@@ -12,36 +12,95 @@ namespace mockup1_848
 {
     public partial class Form2 : Form
     {
-        
+        obtenerValores usuarios = new obtenerValores();
         public Form2()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            datos.DataSource = CGlobal.listaValores;
+            datos.Columns[0].HeaderText = "NOMBRE";
+            datos.Columns[0].Width = 150;
+            datos.Columns[1].HeaderText = "FECHA";
+            datos.Columns[1].Width = 150;
+            datos.Columns[2].HeaderText = "TELEFONO";
+            datos.Columns[2].Width = 150;
+            datos.Columns[3].HeaderText = "CEDULA";
+            datos.Columns[3].Width = 150;
+            datos.Columns[4].HeaderText = "COMPUT";
+            datos.Columns[4].Width = 150;
+            datos.Columns[5].HeaderText = "DESCRIPCION";
+            datos.Columns[5].Width = 150;
+            datos.Columns[6].HeaderText = "COSTO";
+            datos.Columns[6].Width = 150;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label43_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
         {
             this.Hide();
             Form1 registro = new Form1();
             registro.ShowDialog();
             this.Close();
         }
+
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
         }
 
-        private void Form2_Load(object sender, EventArgs e)
+        private void finalizado_Click(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = CGlobal.listaValores;
-            dataGridView1.Columns[0].HeaderText = "Nombre";
-            dataGridView1.Columns[1].HeaderText = "Fecha";
-            dataGridView1.Columns[2].HeaderText = "Telefono";
-            dataGridView1.Columns[3].HeaderText = "Cedula";
-            dataGridView1.Columns[4].HeaderText = "Comput";
-            dataGridView1.Columns[5].HeaderText = "Descripción";
-            dataGridView1.Columns[5].Width = 150;
-            dataGridView1.Columns[6].HeaderText = "Costo";
+            obtenerValores ag = CGlobal.listaValores[datos.CurrentRow.Index];
+            CGlobal.listaValores.Remove(ag);
+            datos.DataSource = null;
+            datos.Update();
+            datos.Refresh();
 
+            datos.DataSource = CGlobal.listaValores;
+            datos.Columns[0].HeaderText = "NOMBRE";
+            datos.Columns[0].Width = 150;
+            datos.Columns[1].HeaderText = "FECHA";
+            datos.Columns[1].Width = 150;
+            datos.Columns[2].HeaderText = "TELEFONO";
+            datos.Columns[2].Width = 150;
+            datos.Columns[3].HeaderText = "CEDULA";
+            datos.Columns[3].Width = 150;
+            datos.Columns[4].HeaderText = "COMPUT";
+            datos.Columns[4].Width = 150;
+            datos.Columns[5].HeaderText = "DESCRIPCION";
+            datos.Columns[5].Width = 150;
+            datos.Columns[6].HeaderText = "COSTO";
+            datos.Columns[6].Width = 150;
+        }
+
+        private void pendiente_Click(object sender, EventArgs e)
+        {
         }
     }
 }
